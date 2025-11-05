621cc69a6bc0fd9cb374bc5183549df19aa6050e
SayonBiswas <sayonbiswas31@gmail.com>
2025-11-05 13:04:04 +0530

    New Codes

--- MyProject/settings.py
+++ MyProject/settings.py
@@ -73,16 +73,16 @@ WSGI_APPLICATION = 'MyProject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+import os
+import dj_database_url
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'Project',     # ✅ your MySQL database name
-        'USER': 'root',        # your MySQL username
-        'PASSWORD': '1234',    # your MySQL password
-        'HOST': 'localhost',
-        'PORT': '3306',
-    }
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL'),
+        conn_max_age=600
+    )
 }
+
 
 
 
